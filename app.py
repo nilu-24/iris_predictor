a64d188099127bde8018bfa4d9c593507baf09a1
Fardin Abdullah <73608018+nilu-24@users.noreply.github.com>
2021-07-07 03:58:36 +0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,60 @@
+from altair.vegalite.v4.schema.channels import X
+import streamlit as st
+#importing necessary libraries
+from sklearn.datasets import load_iris
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+import seaborn as sb
+
+st.title("Iris Flower Predictor")
+st.sidebar.header("Enter Your Data: ")
+
+def user_input():
+    sepal_length =st.sidebar.slider('Sepal Length',0.0,10.0,5.0)
+    sepal_width = st.sidebar.slider('Sepal Width',0.0,10.0,5.0)
+    petal_length =st.sidebar.slider('Petal Length',0.0,10.0,5.0)
+    petal_width =st.sidebar.slider('Petal Width',0.0,10.0,5.0)
+    data = {"Sepal Length":sepal_length,
+    "Sepal Width": sepal_width,
+    "Petal Length": petal_length,
+    "Petal Width": petal_width
+    }
+    features = pd.DataFrame(data, index=[0])
+    return features
+df = user_input()
+
+st.subheader("User Input")
+st.write(df)   
+
+flowers = load_iris()
+X = flowers.data
+Y = flowers.target
+model = LogisticRegression()
+model.fit(X,Y)
+predicted_flower = model.predict(df)
+pred_values = model.predict(X)
+score = str(model.score(X,Y)*100)
+
+st.subheader("Class Labels with Index")
+st.write(flowers.target_names)
+
+st.subheader("Predicted Flower:")
+st.write(pd.DataFrame(flowers.target_names[predicted_flower]))
+
+st.subheader("Model Accuracy:")
+st.write(score + "%")
+
+
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+st.subheader("Confustion Matrix:")
+cm = confusion_matrix(Y,pred_values)
+sb.heatmap(cm,annot=True)
+plt.xlabel("Predicted")
+plt.ylabel("True")
+st.pyplot()
+
